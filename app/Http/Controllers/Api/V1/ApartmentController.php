284c77a061102c5d0528bf9f7b821c9c308d6003
FinalFantaser <nameunknown@yandex.ru>
2022-11-09 14:28:39 +0400

    Исправлен полнгый ресурс модели Apartment

--- app/Http/Controllers/Api/V1/ApartmentController.php
+++ app/Http/Controllers/Api/V1/ApartmentController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApartmentSearchRequest;
 use App\Services\Service;
 use Illuminate\Http\Request;
 
 use App\Query\Blog\Apartment\Sale as FindForSaleQuery;
 use App\Query\Blog\Apartment\Rent as FindForRentQuery;
 use App\Query\Blog\Apartment\ById as FindByIdQuery;
+use App\Query\Blog\Apartment\SaleWithFilters;
 
 class ApartmentController extends Controller
 {
@@ -22,10 +24,22 @@ class ApartmentController extends Controller
         return $this->service->query(new FindForSaleQuery);
     } //sale
 
+    public function saleFiltered(ApartmentSearchRequest $request) //Загрузка квартир на продажу с фильтрацией
+    {
+        return $this->service->query(
+            new SaleWithFilters($request)
+        );
+    } //saleFiltered
+
     public function rent() //Загрузка квартир для аренды
     {
         return $this->service->query(new FindForRentQuery);
     } //rent
+
+    public function rentFiltered(ApartmentSearchRequest $request) //Загрузка квартир для аренды с фильтрацией
+    {
+        
+    } //rentFiltered
 
     public function show(int $apartment) //Загрузка полной информации по квартире
     {
